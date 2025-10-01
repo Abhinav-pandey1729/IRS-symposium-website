5c0cac45792668c2c6c19af7f903da112a3763bc
Abhinav Kumar Pandey <166020326+Abhinav-pandey1729@users.noreply.github.com>
2025-10-01 09:34:52 +0530

    Updated gallery-section.tsx to replace the Rick Astley video with two videos side by side.

--- src/components/gallery-section.tsx
+++ src/components/gallery-section.tsx
@@ -1,4 +1,31 @@
-<div style="display: flex;">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/XBi3ZuDLm70" frameborder="0" allowfullscreen></iframe>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/8IxRjPNg3OI" frameborder="0" allowfullscreen></iframe>
-</div>
+import React from 'react';
+
+const GallerySection: React.FC = () => {
+    return (
+        <div className="gallery-section">
+            <h2>Video Gallery</h2>
+            <div className="video-container" style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/XBi3ZuDLm70"
+                    title="YouTube video"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/8IxRjPNg3OI"
+                    title="YouTube video"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            </div>
+        </div>
+    );
+};
+
+export default GallerySection;
